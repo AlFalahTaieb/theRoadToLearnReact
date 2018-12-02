e2af6f1189f3388f65082a0bfd50208b17bc7de6
AlFalah <taiebalfalah@gmail.com>
2018-12-02 23:49:52 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,98 +1,169 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Search from './Search'
+import Table from './Table'
+import Button from './Button'
+import axios from 'axios'
+
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+const PATH_BASE = "http://hn.algolia.com/api/v1";
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage='
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
+
+console.log(url);
 
 
-class understandBinding extends Component {
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
+class App extends Component {
+  // _isMounted = false;
+
   constructor(props) {
     super(props);
-    this.onClickMe = this.onClickMe.bind(this)
-  }
-  onClickMe() {
-    console.log('hey Oh :) ')
-  }
-  render() {
-    return(
-    <button
-      onClick={this.onClickMe}
-      type="button"
-    >
-      Click Me
-</button>
-    )
-  }
 
-}
-
-let hello = "Going to Learn React :) Mr  "
-let person = {
-  name: 'Taieb',
-  nam: 'AlFalah',
-  job: 'Developer JS',
-  age: 25
-}
-const userService = {
-  getUserName(person) {
-    return person.name + '' + person.nam
-  }
-}
-
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org',
-    author: 'Jordan Walke',
-    num_comments: '2',
-    points: 4,
-    objectID: 2
-  },
-  {
-    title: 'ReDux',
-    url: 'https://redux.js.org',
-    author: 'Dan Abramov, Andrew Clar',
-    num_comments: '2',
-    points: 5,
-    objectID: 1
-  }]
-class App extends Component {
-  constructor(props) {
-    super(props)
     this.state = {
-      list,
-      birth: person.age,
-    }
-    this.onDismiss = this.onDismiss.bind(this)
+      results: null,
+      searchKey: '',
+      searchTerm: DEFAULT_QUERY,
+      error: null
+    };
+    this.needToSearchTopStories = this.needToSearchTopStories.bind(this);
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.onDismiss = this.onDismiss.bind(this);
   }
+
+  needToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  setSearchTopStories(result) {
+    this.setState({ result });
+  }
+
   onDismiss(id) {
-    const updatedList = this.state.list.filter(item => item.objectID !== id)
-    this.setState({ list: updatedList })
-    console.log(id)
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits }
+      }
+    })
   }
 
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+      .then(result => this._isMounted && this.setSearchTopStories(result.data))
+      .catch(error => this._isMounted && this.setState({ error }));
+  }
+
+  componentDidMount() {
+    this._isMounted = true
+    const { searchTerm } = this.state
+    this.setState({ searchKey: searchTerm })
+    this.fetchSearchTopStories(searchTerm)
+  }
+
+  componentWillUnmount() {
+    this._isMounted = false
+  }
+
+  onSearchSubmit(event) {
+    // axios.get('https://www.reddit.com/r/GoneWildHairy/top.json', { responseType: 'json' }).then(response => {
+    //   const tab = (response.data.data.children)
+    //   console.log(tab)
+    //   for (let i = 0; i < tab.length; i++) {
+    //     console.log(tab[i].data.url)
+    //   }
+    // });
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm })
+    if (this.needToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+    event.preventDefault();
+  }
+
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits : [];
+
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
   render() {
+    const {
+      searchTerm,
+      results,
+      searchKey,
+      error
+    } = this.state;
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
+    if (!results) { return null; }
     return (
-
-      <div className="App" >
-        <h1>{hello} {person.name} {person.job} {this.state.birth}</h1>
-        {this.state.list.map(item => {
-          return (
-            <div key={item.objectID}>
-              <span> <a href={item.url} > {item.title} </a> </span>,
-  <span> {item.author} </span>,
-  <span> {item.num_comments} </span>,
-  <span> {item.points} </span>
-              <span>
-                <button onClick={() => this.onDismiss(item.objectID)}
-                  type='button' >Dismiss</button>
-              </span>
-            </div>
-          )
-        })}
-        < p > Many people always ask me how I was able to get into Harvard as a 16 year old who skipped 3 grades of high school.They think I got in because of my scholarly records, but no the key is the interview.As I sat in the Harvard Dean's office in front of the board of reviewers for my application, the Dean asks me "Why should you be a good candidate for this school?" They seemed bored but I replied "Well I was born a child prodigy, placed 1st in my state spelling bee for three consecutive years, I can speak eight different languages not counting Latin, play four different instruments, I skipped grades 4 through 6, and graduated my high school as valedictorian at the age of 14. I then worked as an intern at both Telsa, and NASA." Suddenly the room burst into laughter and many of board instantly started scribbling down "No" near the application check marks. The Dean says "Sorry but you are just not the type we are looking for." But then I said "Excuse me but I wasn't finished...I watch Rick and Morty" The Dean looked at me like an idiot and said "So....?" Then I replied with a smile "And I understand all the references and subtle jokes" An audible gasp let out by the board was so loud the secretary had to come in. You could hear a pin drop and then suddenly all at once the entire board clicked their pens on the "Approved Box" and I was instantly handed a diploma and now I'm teaching advanced physicals there. I guess you can say I'm pretty smart. :)</p>
-
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+      </Search>
+        </div>
+        {error
+          ? <div className='interactions'>
+            <p>Something wen wrong.</p>
+          </div>
+          : <Table
+            list={list}
+            onDismiss={this.onDismiss}
+          />
+        }
+        <div className='interactions'>
+          <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>
+            More
+        </Button>
+        </div>
       </div>
-
-    )
+    );
   }
 }
 
---
+++ src/corquis.js
@@ -0,0 +1,190 @@
+import React, { Component } from "react";
+import "./App.css";
+
+const largeColumn = {
+  width: "40%"
+};
+const midColumn = {
+  width: "30%"
+};
+const smallColumn = {
+  width: "10%"
+};
+
+// function isSearched(searchTerm) {
+//   return function(item) {
+//     return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+//   };
+// }
+
+// const isSearched = searchTerm => item =>
+//   item.title.toLowerCase().includes(searchTerm.toLowerCase());
+
+const DEFAULT_QUERY = "redux";
+const DEFAULT_HPP = 5;
+
+const PATH_BASE = "https://hn.algolia.com/api/v1";
+const PATH_SEARCH = "/search";
+const PARAM_SEARCH = "query=";
+const PARAM_PAGE = "page=";
+const PARAM_HPP = "hitsPerPage=";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: null,
+      searchTerm: DEFAULT_QUERY,
+      searchKey: "",
+      results: null
+    };
+    this.onDismiss = this.onDismiss.bind(this);
+    this.onSearchChange = this.onSearchChange.bind(this);
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
+    this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+  }
+
+  needsToSearchTopStories(searchTerm) {
+    return !this.state.results[searchTerm];
+  }
+
+  setSearchTopStories(result) {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+    // const oldHits = page !== 0 ? this.state.result.hits : [];
+    const oldHits =
+      results && results[searchKey] ? results[searchKey].hits : [];
+    const updatedHits = [...oldHits, ...hits];
+
+    // this.setState({ result: { hits: updatedHits, page } });
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  fetchSearchTopStories(searchTerm, page = 0) {
+    const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm} \
+                 &${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`;
+    console.log(url);
+    fetch(url)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => console.log(e));
+  }
+
+  onSearchSubmit(event) {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+
+    if (this.needsToSearchTopStories(searchTerm)) {
+      this.fetchSearchTopStories(searchTerm);
+    }
+
+    event.preventDefault();
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    this.fetchSearchTopStories(searchTerm);
+  }
+
+  onSearchChange(event) {
+    this.setState({ searchTerm: event.target.value });
+  }
+
+  onDismiss(id) {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter(isNotId);
+
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  render() {
+    const { results, searchTerm, searchKey } = this.state;
+    const page =
+      (results && results[searchKey] && results[searchKey].page) || 0;
+    const list =
+      (results && results[searchKey] && results[searchKey].hits) || [];
+
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={searchTerm}
+            onChange={this.onSearchChange}
+            onSubmit={this.onSearchSubmit}
+          >
+            Search
+          </Search>
+        </div>
+        <Table list={list} onDismiss={this.onDismiss} />
+
+        <div className="interactions">
+          <Button
+            onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+          >
+            More
+          </Button>
+        </div>
+      </div>
+    );
+  }
+}
+
+const Search = ({ value, onChange, onSubmit, children }) => {
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} />
+      <button type="submit">{children}</button>
+    </form>
+  );
+};
+
+const Table = ({ list, onDismiss }) => {
+  return (
+    <div className="table">
+      {list.map(item => (
+        <div key={item.objectID} className="table-row">
+          <span style={largeColumn}>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span style={midColumn}>{item.author}</span>
+          <span style={smallColumn}>{item.num_comments}</span>
+          <span style={smallColumn}>{item.points}</span>
+          <span style={smallColumn}>
+            <Button
+              onClick={() => onDismiss(item.objectID)}
+              className="button-inline"
+            >
+              Dismiss
+            </Button>
+          </span>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const Button = ({ onClick, className = "", children }) => {
+  return (
+    <button onClick={onClick} className={className} type="button">
+      {children}
+    </button>
+  );
+};
+
+export default App;
